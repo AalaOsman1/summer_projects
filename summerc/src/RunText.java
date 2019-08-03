9f1a616bfa0b24665f64f3f113ab71c6a495ff6b
Aosman <48081743+AalaOsman1@users.noreply.github.com>
2019-08-03 16:15:35 +0100

    Done

--- summerc/src/RunText.java
+++ summerc/src/RunText.java
@@ -1,10 +1,10 @@
-
+    // this class will run the text on a text document
 public class RunText {
     public static void main(String args[]) {
         ReadText ro = new ReadText();
         ro.openFile();
         ro.closeFile();
 
-        // this class will run the text on a text document
+    
     }
 }
