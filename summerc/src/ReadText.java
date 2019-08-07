6ff11d88393591c6d69a580a4c71828cd868e32b
Aosman <48081743+AalaOsman1@users.noreply.github.com>
2019-08-07 17:09:13 +0100

    Done

--- summerc/src/ReadText.java
+++ summerc/src/ReadText.java
@@ -1,4 +1,4 @@
-//Main class
+
 import java.io.File;
 import java.util.Scanner;
 import java.io.*;
@@ -7,16 +7,36 @@ public class ReadText extends HelloWorld {
 
     // public variables
     public String a;
+    public String op;
 
     // Scanners
     public Scanner input = new Scanner(System.in);
+    public Scanner menuo = new Scanner(System.in);
+
+    public void menu() throws FileNotFoundException {
+        System.out.println("enter what you want,count,read");
+        String op = menuo.nextLine();
+        switch (op) {
+        case "read":
+            System.out.println("this will read the content of the file opened");
+            readFile();
+            break;
+        case "count":
+            System.out.println("this will count words");
+            count();
+            break;
+        default:
+            System.out.println("Invalid option. Please try again...");
+            break;
+        }
+    }
 
     public void openFile() {
         try {
             System.out.println("enter name of file");
             File file = new File(input.nextLine());
             input = new Scanner(file);// finding a already made file
-            readFile();
+            menu();
         } catch (Exception e) {
             System.out.println("can't find file");
         }
@@ -30,7 +50,18 @@ public class ReadText extends HelloWorld {
 
     }
 
+    public void count() throws FileNotFoundException {
+        int count = 0;
+        while (input.hasNext()) {
+            String x = input.next();
+            if (x.indexOf("\\") == -1)
+                count++;
+        }
+        System.out.println(" number of words on this text file is " + count);
+    }
+
     public void closeFile() {
         input.close();
     }
+
 }
--- summerc/src/RunText.java
+++ summerc/src/RunText.java
@@ -1,8 +1,9 @@
-  
+
 public class RunText {
     public static void main(String args[]) {
         ReadText ro = new ReadText();
+
         ro.openFile();
-        ro.closeFile();
+
     }
 }
